55c0c6da9310a67d1415dc9777fdfc9886e0ebc0
Jesse Copeland <jesse.copeland@gmail.com>
2015-02-20 11:18:26 -1000

    site styling complete

--- public/js/app.js
+++ public/js/app.js
@@ -6,10 +6,11 @@ function Location () {
   };
 }
 
-Location.prototype.fetchWeatherData = function (airportCode) {
+Location.prototype.fetchWeatherData = function (airportCode, callback) {
   var _location = this;
   var queryStub = '/conditions/almanac/q/';
   var fullQuery = _location.wuApi.urlStub + _location.wuApi.apiKey + queryStub + airportCode + _location.wuApi.resType;
+  
   $.ajax({
     url: fullQuery,
     dataType: 'jsonp',
@@ -23,24 +24,50 @@ Location.prototype.fetchWeatherData = function (airportCode) {
       _location.lowTempRecord = parsedJson['almanac']['temp_low']['record']['F'];
       _location.lowTempYear = parsedJson['almanac']['temp_low']['recordyear'];
 
-      console.log(_location);
+      callback(_location);
+
     } // end success function
   }); //end ajax call
 };
 
+Location.prototype.getData = function () {
+  var returnObj = {
+    city:           this.city,
+    currentTemp:    this.currentTemp,
+    highTempNormal: this.highTempNormal,
+    highTempRecord: this.highTempRecord,
+    highTempYear:   this.highTempYear,
+    lowTempNormal:  this.lowTempNormal,
+    lowTempRecord:  this.lowTempRecord,
+    lowTempYear:    this.lowTempYear
+  };
+  return returnObj;
+};
+
+function attachAttributes ($el, weatherData, callback) {
+  $el.data('weatherData', weatherData.getData());
+  callback($el);
+}
+
+function render ($element) {
+  $element.find('h1').text($element.data('weatherData').city);
+  $element.find('figure').text($element.data('weatherData').currentTemp);
+
+}
+
 $(function () {
-
+  var locationElement = $('main.weather');
   var location = new Location();
-
   var citySelects = $('.city-select .cs-options');
-
   var selectedAirport;
     
     citySelects.on('click', 'li', function () {
       selectedAirport = $(this).data('value');
-      // location.fetchCurrentObservation(selectedAirport);
-      // location.fetchLocationHighsLows(selectedAirport);
-      location.fetchWeatherData(selectedAirport);
+      location.fetchWeatherData(selectedAirport, function (location) {
+        attachAttributes(locationElement, location, function ($element) {
+          render($element);
+        });
+      });
     });
 
 }); //end jQuery function
